230c5e846c0d7db9c9f3244c41cb14b9c1ae8c4f
Shankar <shankar.n@ethnus.com>
2025-08-07 12:56:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Logout from './pages/Logout';
+import UserInfo from './pages/UserInfo';  // Protected page
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import { TaskBoard } from './pages/TaskBoard';
+import CreateTask from './pages/CreateTask';
+import EditTask from './pages/EditTask';
 
 function App() {
+  const isAuthenticated = !!localStorage.getItem('access_token');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={
+          isAuthenticated ? <UserInfo /> : <Home />
+        } />
+        <Route path="/login" element={
+          isAuthenticated ? <Navigate to="/profile" /> : <Login />
+        } />
+
+        <Route path="/tasks" element={
+          isAuthenticated ? <TaskBoard /> : <Login />
+        } />
+
+        <Route path="/edit/:id" element={
+          isAuthenticated ? <EditTask /> : <Login />
+        } />
+
+        <Route path="/create" element={
+          isAuthenticated ? <CreateTask /> : <Login />
+        } />
+
+        <Route path="/register" element={
+          isAuthenticated ? <Navigate to="/" /> : <Register />
+        } />
+        <Route path="/logout" element={
+          isAuthenticated ? <Logout /> : <Navigate to="/login" />
+        } />
+        {/* You can add more routes here */}
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/api/axiosConfig.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'https://taskmanager-1dgi.onrender.com/api/',
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default axiosInstance;
